3809fd899fa09b6e15ebb9a0160d8bf27ecd225e
Val <dgm97@cornell.edu>
2016-11-25 21:05:16 -0500

    unblock em text

--- tooltip.js
+++ tooltip.js
@@ -31,6 +31,7 @@ function biCleaner() {
 function addLoader(){
 	var loader = document.createElement("canvas");
 	loader.id = "_loader";
+	loader.innerHTML="UnblockEm";
 	document.body.appendChild(loader);
 	var st = loader.style;
 	st.position = "absolute";
