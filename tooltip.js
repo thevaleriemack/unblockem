8d4358c77bcdb8956499b9d7fa2153c3047f350e
Val <dgm97@cornell.edu>
2016-11-25 20:47:26 -0500

    added js loader

--- tooltip.js
+++ tooltip.js
@@ -2,11 +2,12 @@ function quoraCleaner() {
 	var wrppr = document.getElementsByClassName("vertical_alignment_wrapper")[0];
 	var bdy = document.getElementsByTagName("BODY")[0];
 	if (wrppr == bdy == null) {
+		console.log('could not find elements');
+	} else {
 		wrppr.parentNode.removeChild(wrppr);
+		bdy.style.overflow = "scroll";
 		bdy.className = bdy.className.replace( /(?:|\s)gating-new_signup-on signup_wall_prevent_scroll(?!\S)/g , '' )
 		console.log('quoraCleaner ran');
-	} else {
-		console.log('could not find elements');
 	}
 }
 
@@ -20,13 +21,58 @@ function biCleaner() {
 	} else {
 		ifrm.parentNode.removeChild(ifrm);
 		bckdrp.parentNode.removeChild(bckdrp);
-		bdy.setAttribute("style", "overflow: scroll");
+		bdy.style.overflow = "scroll";
 		rcc.parentNode.removeChild(rcc);
 		console.log('biCleaner ran');
 	}
 }
 
+// loader
+function addLoader(){
+	var loader = document.createElement("canvas");
+	loader.id = "_loader";
+	document.body.appendChild(loader);
+	var st = loader.style;
+	st.position = "absolute";
+	st.margin = "100px auto";
+	st.top = "0px";
+	st.width = "100px";
+	st.zIndex = "10";
+}
+function animateLoader() {
+	var cnvs = document.getElementById("_loader");
+	var ctx = cnvs.getContext('2d');
+	
+	var start = new Date();
+	var lines = 500, cW = cnvs.width, cH = cnvs.height;
+	var draw = function() {
+		var rotation = parseInt(((new Date() - start) / 1800) * lines) / lines;
+	    ctx.save();
+	    ctx.clearRect(0, 0, cW, cH);
+	    ctx.translate(cW / 2, cH / 2);
+	    ctx.rotate(Math.PI * 2 * rotation);
+	    for (var i = 0; i < lines; i++) {
+	    	ctx.beginPath();
+	        ctx.rotate(Math.PI * 2 / lines);
+	        ctx.moveTo(cW / 10, 0);
+	        ctx.lineTo(cW / 4, 0);
+	        ctx.lineWidth = cW / 30;
+	        ctx.strokeStyle = "rgba(255,200,155," + i / lines + ")";
+	        ctx.stroke();
+	    }
+	    ctx.restore();
+	};
+	var interval = setInterval(draw, 1000 / 30);
+	return interval;
+}
+function removeLoader() {
+	var cnvs = document.getElementById("_loader");
+	cnvs.parentNode.removeChild(cnvs);
+}
+
 function unblockEm() {
+	addLoader();
+	var loaderInterval = animateLoader();
 	var url = window.location.href;
 	var timesRun = 0;
 	// runs every 3 seconds, 10 times total
@@ -35,6 +81,8 @@ function unblockEm() {
 		if (timesRun > 10){
 			console.log("cleaner stopped after 10 runs for diligence");
 			clearInterval(interval);
+			clearInterval(loaderInterval);
+			removeLoader();
 			return;
 		}
 		if (url.includes('quora.com')) {
